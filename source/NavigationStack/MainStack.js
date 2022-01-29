f5ccf045a104cc4bdb87a9383776ad6fc5275259
[Saahith Shetty] <[shettysaahith123@gmail.com]>
2022-01-29 10:49:03 +0530

    added deeplinking

--- source/NavigationStack/MainStack.js
+++ source/NavigationStack/MainStack.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import BottomNav from "./BottomStack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -8,11 +9,47 @@ import LoginScreen from "../Screen/Login/LoginScreen";
 import StartupScreen from "../Screen/StartupScreen";
 import NumberVerification from "../Screen/Login/NumberVerificationScreen";
 import VehicleSelectScreen from "../Screen/VehicleSelect/VehicleSelectScreen";
+import * as Linking from "expo-linking";
+
+const prefix = Linking.createURL("/");
 
 const FlowStack = createStackNavigator();
 const AppNav = () => {
+  //data passed through the Link
+  const [data, setData] = useState(null);
+  console.log(prefix);
+  const linking = {
+    prefixes: [prefix],
+    config: {
+      screens: {
+        Startup: "Startup",
+      },
+    },
+  };
+
+  //handling DeepLinking
+  const handleDeepLink = (event) => {
+    let data = Linking.parse(event.url);
+    setData(data);
+  };
+
+  useEffect(() => {
+    const getInitialURL = async () => {
+      const initialURL = await Linking.getInitialURL();
+      if (initialURL) {
+        setData(Linking.parse(initialURL));
+      }
+    };
+    Linking.addEventListener("url", handleDeepLink);
+    if (!data) {
+      getInitialURL();
+    }
+    return () => {
+      Linking.removeEventListener("url");
+    };
+  }, []);
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <FlowStack.Navigator screenOptions={{ headerShown: false }}>
         <FlowStack.Screen component={StartupScreen} name="Startup" />
         <FlowStack.Screen component={LoginScreen} name="Login" />
